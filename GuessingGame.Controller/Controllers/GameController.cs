2e558f01ee1efeadfbdf34a06b7dd19e16e5865b
Gilgameshh73 <agostonandras51@gmail.com>
2024-07-17 21:07:01 +0200

    Create endpoint for communication with frontend.

---
+++ GuessingGame.Controller/Controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using GuessingGame.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuessingGame.Controller.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GameController : ControllerBase
+    {
+        private readonly GameService _service;
+
+        public GameController(GameService gameService) => this._service = gameService;
+
+        [HttpPost("guess")]
+        public IActionResult MakeGuess([FromBody] int guess)
+        {
+            var result = this._service.MakeGuess(guess);
+            return Ok(result);
+        }
+    }
+}
---
+++ GuessingGame.Controller/Startup.cs
@@ -0,0 +1,66 @@
+﻿using GuessingGame.Controller.Service;
+using GuessingGame.Repository.Classes;
+using GuessingGame.Repository.Interfaces;
+using GuessingGame.Service;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace GuessingGame.Controller
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+
+            services.AddSingleton<IGameRepository, GameRepository>();
+            services.AddTransient<GameService>();
+            services.AddSignalR();
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
+
+            app.UseCors(c => c
+            .AllowCredentials()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:16968"));
+
+            app.UseRouting();
+            app.UseCors("AllowAll");
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<SignalRHub>("/hub");
+            });
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
