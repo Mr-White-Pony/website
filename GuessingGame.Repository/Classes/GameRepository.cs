e699071aaa6af8febf8754000a97648215d29590
Gilgameshh73 <agostonandras51@gmail.com>
2024-07-17 21:06:09 +0200

    Create repository to communicate with the model layer.

---
+++ GuessingGame.Repository/Classes/GameRepository.cs
@@ -0,0 +1,14 @@
+﻿using GuessingGame.Models;
+using GuessingGame.Repository.Interfaces;
+
+namespace GuessingGame.Repository.Classes
+{
+    public sealed class GameRepository : IGameRepository
+    {
+        private GameModel _game = new() { NumberToGuess = new Random().Next(0, 10), NumberOfTries = 0 };
+
+        public GameModel GetGame() => this._game;
+
+        public void SaveGame(GameModel game) => this._game = game;
+    }
+}
---
+++ GuessingGame.Repository/Interfaces/IGameRepository.cs
@@ -0,0 +1,10 @@
+﻿using GuessingGame.Models;
+
+namespace GuessingGame.Repository.Interfaces
+{
+    public interface IGameRepository
+    {
+        GameModel GetGame();
+        void SaveGame(GameModel game);
+    }
+}
