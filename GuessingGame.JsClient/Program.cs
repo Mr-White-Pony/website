e74d9d8e4e4eae7c924e6b1b25d4ebce3de68682
Gilgameshh73 <agostonandras51@gmail.com>
2024-07-17 21:07:35 +0200

    Add frontend (client) layer.

---
+++ GuessingGame.JsClient/Program.cs
@@ -0,0 +1,19 @@
+using GuessingGame.Controller;
+
+namespace GuessingGame.JsClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ GuessingGame.JsClient/wwwroot/script.js
@@ -0,0 +1,1 @@
+﻿// Scripting happens here
